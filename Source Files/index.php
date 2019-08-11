454911e43600a4962ee32011628be29346822214
Hannu Kauppinen <Hannu Kauppinen@DESKTOP-AKLH3SS>
2019-08-11 16:37:15 +0300

    Test commit

---
+++ Source Files/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>Simple tool to merge several IOF Interface Standard CourseData XML files</h1>
+        <p>The purpose is to creata a simple system to create an archive of all control locations used for a particular map. This enable course planners to avoid or reuse previously used control locations.</p>
+
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
